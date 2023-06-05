4c421e2d8520e5f72f3471f01350fe81455b7e10
Maxim Lesy <maxim.lesy@howest.be>
2023-06-05 13:10:09 +0200

    Add project files.

---
+++ Mde.Mvvm.StudentRoulette/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Mde.Mvvm.StudentRoulette;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ Mde.Mvvm.StudentRoulette/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace Mde.Mvvm.StudentRoulette;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
