e8830daf0e2f7a6960688c8c427038db044c81c6
Maxim Lesy <maxim.lesy@howest.be>
2023-06-28 21:55:40 +0200

    Fix indentation in MauiApp

--- Mde.Mvvm.StudentRoulette/MauiProgram.cs
+++ Mde.Mvvm.StudentRoulette/MauiProgram.cs
@@ -7,27 +7,27 @@ namespace Mde.Mvvm.StudentRoulette;
 
 public static class MauiProgram
 {
-public static MauiApp CreateMauiApp()
-{
-	var builder = MauiApp.CreateBuilder();
-	builder
-		.UseMauiApp<App>()
-		.ConfigureFonts(fonts =>
-		{
-			fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-			fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-		});
+	public static MauiApp CreateMauiApp()
+	{
+		var builder = MauiApp.CreateBuilder();
+		builder
+			.UseMauiApp<App>()
+			.ConfigureFonts(fonts =>
+			{
+				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+			});
 
-	builder.Services.AddTransient<StudentFormPage>();
-	builder.Services.AddTransient<StudentFormViewModel>();
+		builder.Services.AddTransient<StudentFormPage>();
+		builder.Services.AddTransient<StudentFormViewModel>();
 		
-	builder.Services.AddTransient<StudentListPage>();
-	builder.Services.AddTransient<StudentListViewModel>();
+		builder.Services.AddTransient<StudentListPage>();
+		builder.Services.AddTransient<StudentListViewModel>();
 
-	Routing.RegisterRoute(nameof(StudentFormPage), typeof(StudentFormPage));
+		Routing.RegisterRoute(nameof(StudentFormPage), typeof(StudentFormPage));
 
-	builder.Services.AddTransient<IStudentService, JsonStudentService>();
+		builder.Services.AddTransient<IStudentService, JsonStudentService>();
 
-	return builder.Build();
-}
+		return builder.Build();
+	}
 }
